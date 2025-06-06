83202dfdc2b530bfe0a2272abcc4a9bec5e0ac6e
RavjitSingh2000 <ravjitsingh1221@gmail.com>
2025-06-06 02:02:12 -0400

    Project (2/25): Random Color Generator complete

--- README.md
+++
@@ -1,2 +0,0 @@
-# react-dummy-projects
-Basic React projects to from from zero to hero.
--- react-projects/src/components/RandomColorGenerator/index.jsx
+++ react-projects/src/components/RandomColorGenerator/index.jsx
@@ -1,21 +1,62 @@
+import { useState } from "react";
 import "./style.css";
 
 export default function RandomColorGenerator() {
 
+    const [colorType, setColorType] = useState('rgb');
+    const [defColor, setDefColor] = useState(`#ffffff`);
+
+    const handleRandomColor = () => {
+        switch (colorType) {
+            case "hex":
+                setDefColor(randomHexColor());
+                break;
+            case "rgb":
+                setDefColor(randomRGBColor());
+                break;
+        }
+    }
 
 
     return (
         <div className="main-container">
             <nav className="main-nav">
-                <ul>
-                    <li><button>Generate Random Color</button></li>
-                    <li><button>Genrate Hex Color</button></li>
-                    <li><button>Generate RGB Colors</button></li>
-                </ul>
+                <button onClick={() => handleRandomColor()}>Generate Random Color</button>
+                <button onClick={() => setColorType('hex')}>Genrate Hex Color</button>
+                <button onClick={() => setColorType('rgb')}>Generate RGB Colors</button>
             </nav>
-            <section>
-
+            <section style={{
+                backgroundColor: defColor
+            }}>
+                <header>
+                    <span>Color type: {colorType}</span>
+                    <span>{defColor}</span>
+                </header>
             </section>
         </div>
     )
+}
+
+
+//util functions
+
+const randomHexColor = () => {
+
+    const hexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
+
+    let hexColor = "#";
+
+    for (let i = 0; i < 6; i++) {
+        hexColor += hexArray[Math.floor(Math.random() * (hexArray.length))];
+    }
+    return hexColor;
+}
+
+const randomRGBColor = () => {
+
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    return `rgb(${r},${g},${b})`;
 }
