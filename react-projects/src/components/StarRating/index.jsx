83d94819f836710008b6f575ff861e2010a0e0d6
RavjitSingh2000 <ravjitsingh1221@gmail.com>
2025-06-09 18:10:02 -0400

    Project (3/25): Star rating added

---
+++ react-projects/src/components/StarRating/index.jsx
@@ -0,0 +1,13 @@
+
+
+function StarRating() {
+    return (
+        <main className="main-wrapper">
+            <section className="stars_wrapper">
+                <div className="star"></div>
+            </section>
+        </main>
+    )
+}
+
+export default StarRating;
