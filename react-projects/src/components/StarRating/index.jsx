44f44805acc9806a4274de82552129ff7a3a35c7
RavjitSingh2000 <ravjitsingh1221@gmail.com>
2025-06-12 01:25:08 -0400

    Project (3/25): Star rating complete

--- react-projects/src/components/StarRating/index.jsx
+++ react-projects/src/components/StarRating/index.jsx
@@ -1,13 +1,42 @@
+import { useState } from "react";
 import "./style.css";
 
-function StarRating() {
+function StarRating({ numOfStars=8 }) {
+
+    const [rating, setRating] = useState(0);
+    const [hoverState, setHoverState] = useState(0);
+
+    const handleMouseClick = (index) => {
+        setRating(index);
+        
+    }
+    const handleMouseHover = (index) => {
+        setHoverState(index);
+    }
+    const handleMouseLeave = () => {
+        hoverState(rating);
+    }
+
+
+
     return (
         <main className="main-wrapper">
             <header className="main-wrapper-header">
                 <p className="header-text">STAR RATING</p>
             </header>
             <section className="stars-wrapper">
-                <div className="star"></div>
+                {
+                    [...Array(numOfStars)].map((i, index) => {
+                        return <div
+                            className={`star ${index <= (hoverState || rating) ? 'star-selected':''}`}
+                            key={index}
+                            onClick={() => handleMouseClick(index)}
+                            onMouseMove={() => handleMouseHover(index)}
+                            onMouseLeave={() => handleMouseLeave()}
+                        >
+                        </div>
+                    })
+                }
             </section>
         </main>
     )
